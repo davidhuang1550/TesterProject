c782c7d29aa865aec1fe77731ec1ecdfec69aa46
xlhuang3 <huangdavidd@gmail.com>
2016-11-03 13:07:32 -0400

    updates

--- app/src/main/java/com/example/xlhuang3/testerproject/MainActivity.java
+++ app/src/main/java/com/example/xlhuang3/testerproject/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         Search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
 
